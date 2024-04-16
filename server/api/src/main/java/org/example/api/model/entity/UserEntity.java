1846879c970869d78927eb4df43741e4c72a90bd
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-04-16 16:36:14 +0300

    creating user entity class

--- server/api/src/main/java/org/example/api/model/entity/UserEntity.java
+++ server/api/src/main/java/org/example/api/model/entity/UserEntity.java
@@ -1,5 +1,6 @@
 package org.example.api.model.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.*;
@@ -12,7 +13,12 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class UserEntity extends BaseEntity{
+    @Column(unique = true, nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String username;
+
+    @Column(nullable = false)
     private String password;
 }
