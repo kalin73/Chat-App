a934c6203f28e99db0b4f799f330aa13c7103e12
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-04-16 16:31:20 +0300

    creating user entity class

---
+++ server/api/src/main/java/org/example/api/model/entity/UserEntity.java
@@ -0,0 +1,20 @@
+package org.example.api.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity extends BaseEntity{
+    private String email;
+    private String username;
+    private String password;
+}
