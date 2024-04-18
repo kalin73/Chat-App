09c5982fe97426163a93a4b93b888b432160d608
Strahil731 <157738598+Strahil731@users.noreply.github.com>
2024-04-18 10:09:58 +0300

    redact components and json

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Login.css";
 
 function Login() {
+    const [data, setData] = useState({
+        username: "",
+        password: ""
+    });
+
+    function handelrUsername(event) {
+        const { value } = event.target;
+        setData({ ...data, "username": value });
+    }
+
+    function handelrPassword(event) {
+        const { value } = event.target;
+        setData({ ...data, "password": value });
+    }
+
+    async function registerUser() {
+        
+    }
+
     return (
         <div className="login-form">
             <h2>Login</h2>
             <form>
-                <input type="text" placeholder="Username" />
+                <input id="username" type="text" placeholder="Username" value={data.username} onChange={handelrUsername} />
                 <br />
-                <input type="password" placeholder="Password" />
+                <input id="password" type="password" placeholder="Password" value={data.password} onChange={handelrPassword} />
                 <br />
-                <button type="submit">Login</button>
+                <button type="submit" onClick={registerUser}>Login</button>
             </form>
         </div>
     );
